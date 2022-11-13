270f576dffd238f4bb444951de5bd82bf39015a8
BrianVL <noppebrian@gmail.com>
2022-11-13 21:17:30 +0100

    Add function getComputerChoice

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function add(a, b) {
-    return a + b;
+function getComputerChoice () {
+    let choice = Math.floor(Math.random()*3);
+    switch (choice) {
+        case 0: 
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2: 
+            return 'scissors';
+    }
 }
