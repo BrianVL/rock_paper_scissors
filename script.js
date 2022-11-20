697c36ff42b296e726755759c59055c964882924
BrianVL <noppebrian@gmail.com>
2022-11-20 15:45:23 +0100

    Add function playRPS to code

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ function getComputerChoice () {
             return 'scissors';
     }
 }
+
+function playRPS (playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return 'Draw';
+    }
+    else if (playerSelection === 'rock' && computerSelection === 'paper'
+    || playerSelection === 'paper' && computerSelection === 'scissors'
+    || playerSelection === 'scissors' && computerSelection === 'rock') {
+        return `You lose! ${computerSelection[0].toUpperCase() + computerSelection.slice(1)} beats ${playerSelection}`;
+    }
+    else if (playerSelection === 'rock' && computerSelection === 'scissors'
+    || playerSelection === 'paper' && computerSelection === 'rock'
+    || playerSelection === 'scissors' && computerSelection === 'paper') {
+        return `You win! ${playerSelection[0].toUpperCase() + playerSelection.slice(1)} beats ${computerSelection}`;
+    }
+    else {
+        return 'Error';
+    }
+}
